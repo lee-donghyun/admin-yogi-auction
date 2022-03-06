83ac9f527b8b23e7f7ff7a61ff25bbe10da379da
lee dong hyun <donghyun.lee@fetching.co.kr>
2022-03-06 19:02:01 +0900

    관리자 계정 추가

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,4 +1,5 @@
 export const users: Admin[] = [
   { id: "test", password: "test", name: "테스트 계정" },
   { id: "dh.lee", password: "qwerty", name: "이동현" },
+  { id: "super", password: "yogiauction", name: "관리자" },
 ];
